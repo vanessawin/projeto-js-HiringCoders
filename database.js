052e77abddf79adf1fdcd414efda9b5221eecc1c
vanessawin <vanessa.win@hotmail.com>
2021-07-13 03:54:17 -0300

    adicioando array de objetos

---
+++ database.js
@@ -0,0 +1,56 @@
+const livros = [
+    {
+        id: 1,
+        nome: "A Cabana",
+        autor: "William P. Young",
+        categoria: "Ficção",
+        paginas: 240,
+        recomenda: true,
+        leu: false
+    },
+    {
+        id: 2,
+        nome: "Pai Rico, Pai Pobre",
+        autor: "Robert Kyosaki",
+        categoria: "Finanças",
+        paginas: 153,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 3,
+        nome: "O Monge e o Executivo",
+        autor: "James C. Hunter",
+        categoria: "Liderança",
+        paginas: 144,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 4,
+        nome: "Javascript e Jquery: desenvolvimento de interfaces web interativas",
+        autor: "Jon Duckett",
+        categoria: "Programação",
+        paginas: "640",
+        recomenda: true,
+        leu: false
+    },
+    {
+        id: 5,
+        nome: "HTML e CSS: projete e constura websites",
+        autor: "Jon Duckett",
+        categoria: "Programação",
+        paginas: 512,
+        recomenda: true,
+        leu: false
+    },
+    {
+        id: 6,
+        nome: "Geração de valor",
+        autor: "Flávio Augusto da Silva",
+        categoria: "Finanças",
+        paginas: 208,
+        recomenda: true,
+        leu: true
+    }
+]
