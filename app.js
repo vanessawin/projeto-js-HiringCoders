7c15c2e03bcf4a2769c0c27211ecf99dd4b9aba8
vanessawin <vanessa.win@hotmail.com>
2021-07-15 06:46:10 -0300

    em construçao

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const livros = require('./database')// importando meus livros do database para app.js
-//console.log(livros)
+console.log(livros)
 
 //pegar o input da pessoa usuaria, se ela quer escolher um livro por categoria 
+//Verificar a capitura do input da entrandaInicial
+//Se for Sim, mostra as categorias disponiveis, e pergunta qual categoria ela escolhe
+//Se for Não, mostra todos os livros em ordem crescente pela quantidade de paginas
 
-const readiline = require("readline-sync") // nome da biblioteca require("readline-sync")
+const readline = require('readline-sync') // nome da biblioteca require("readline-sync")
 // agora da para acessar tudo que a biblioteca disponibiliza à partir da const readiline
 
-const entradaInicial = readiline.question("Deseja buscar um livro?S/N")// Essa propriedade question é nativa readline-sync ela serve para fazer pergunta
+const entradaInicial = readline.question("Deseja buscar um livro? S/N ")// Essa propriedade question é nativa readline-sync ela serve para fazer pergunta
 //para receber a entrada da pessoa usuaria
 
+//Verificar a capitura do input da entrandaInicial
+//Se for Não, mostra todos os livros em ordem crescente pela quantidade de paginas
 
+//Se for Sim, mostra as categorias disponiveis, e pergunta qual categoria ela escolhe
+if (entradaInicial.toLocaleUpperCase() === "S") { // Se a pessoa digitar um s minusculo fazer a transformaçao toUpperCase() === "S"
+    console.log("Essas são as categorias disponiveis: ")
+    console.log("Ficção","/Finanças","/Liderança","/Prgramação")
 
-
-
-
-
-
-
-
+    const entradadaCategoria = readline.question("Qual categoria voce escolhe:")
+    const retorno = livros.filter(livro => livro.categoria === entradadaCategoria)
+    console.log(retorno)
+}
