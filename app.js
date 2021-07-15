8de5fc2e024d976538194e174f7c366164e4ded2
vanessawin <vanessa.win@hotmail.com>
2021-07-15 02:48:49 -0300

    importando livros para app.js, adicionado a biblioteca realine-sync para acessar tudo que ela disponibiliza

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-const livros = require('./database')
-console.log(livros)
+const livros = require('./database')// importando meus livros do database para app.js
+//console.log(livros)
+
+//pegar o input da pessoa usuaria, se ela quer escolher um livro por categoria 
+
+const readiline = require("readline-sync") // nome da biblioteca require("readline-sync")
+// agora da para acessar tudo que a biblioteca disponibiliza à partir da const readiline
+
+
+
+
+
+
+
+
+
+
